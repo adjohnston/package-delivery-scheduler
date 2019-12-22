7db9e21b3b5d7ea72e3c77fb86a2c4ea7b3f77e1
Adam Johnston <666676+adjohnston@users.noreply.github.com>
2019-12-22 13:31:59 +0000

    replace div with React Fragment
    React Fragment's can reduce `div` soup and have cleaner markup overall. You can do it as I have done in the suggestion or destructure as a component like this:
    
    ```
    import React, { Fragment } from react
    
    const App = () => (
      <Fragment>
        …
      </Fragment>
    )
    ```
    
    I believe the above will allow you to add a `key` prop if you're mapping over a list.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import DeliveryHeader from "./components/molecules/delivery-header";
 import DeliveryFooter from "./components/molecules/delivery-footer";
 
 const App = () => (
-  <div>
+  <>
     <DeliveryHeader />
     <DeliveryFooter />
-  </div>
+  </>
 );
 
 export default App;
